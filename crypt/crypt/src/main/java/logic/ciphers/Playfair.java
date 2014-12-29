e5c5a92478790ccb943b1c4c9222a6f2bdf59671
korppi <korppi@users.noreply.github.com>
2014-12-29 22:24:05 +0200

    Lisätty mahdollisuus salauksen purkamiseen, alettu työstää Playfair-salausta ja muokattu käyttöliittymää, sekä dokumentaatiota.

---
+++ crypt/crypt/src/main/java/logic/ciphers/Playfair.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2014 korppi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package logic.ciphers;
+
+import static misc.AlphabetArray.alphabet;
+
+/**
+ *
+ * @author korppi
+ */
+public class Playfair {
+
+    //Avain on 5*5-taulukko, joka sisältää aakkoset välillä a-z j:tä lukuunottamatta
+    //j tulkitaan i:ksi
+    public char[] encrypt(char plain[], char key[][]) {
+        char modifiedPlain[] = jToI(plain);
+        //Johtuen tietyistä playfair-salauksen ominaisuuksista, salattu muoto voi olla pidempi kuin alkuperäinen
+        char cipher[] = new char[plain.length]; //TODO: Kehitä jotain pituuden kanssa
+        
+        /*
+        //Käytetään while-rakennetta, sillä i voi muuttua useita kertoja iteraation aikana.
+        int i = 0;
+        while (i < modifiedPlain.length) {
+            char c[] = new char[2];
+            for (int pairMember = 0; pairMember < 2; pairMember++) {
+                while (true) {
+                    c[pairMember] = plain[i];
+                    if (cipherable(c[pairMember])) {
+                        break;
+                    } else {
+                        if (i < modifiedPlain.length - 1) {
+                            i++;
+                        } else {
+                            if (pairMember == 1) {
+                                //Lisää tarvittaessa loppuun q
+                                c[pairMember] = 'q';
+                            }
+                            break;
+                        }
+                    }
+                }
+                i++;
+            }
+            c = playfairAlter(c, key);
+            for (int pairMember = 0; pairMember < 2; pairMember++) {
+                cipher[i + pairMember] = c[pairMember];
+            }
+        }*/
+        return cipher;
+    }
+    //Avain on 5*5-taulukko, joka sisältää aakkoset välillä a-z j:tä lukuunottamatta
+    //j tulkitaan i:ksi
+
+    public char[] decrypt(char cipher[], char key[][]) {
+        //NYI
+        return null;
+    }
+
+    private char[] jToI(char plain[]) {
+        for (int i = 0; i < plain.length; i++) {
+            if (plain[i] == 'j') {
+                plain[i] = 'i';
+            }
+        }
+        return plain;
+    }
+
+    //Käytetään kirjainparin salaamiseen
+    private char[] playfairAlter(char plainPair[], char key[][]) {
+        char cipherPair[] = plainPair;
+        int keyPos[][] = new int[2][2];   //Kirjain (0/1); avaimen X- ja Y- arvot
+        for (int pairMember = 0; pairMember < 2; pairMember++) {
+            for (int x = 0; x < key.length; x++) {
+                for (int y = 0; y < key[x].length; y++) {
+                    if (key[x][y] == cipherPair[pairMember]) {
+                        keyPos[pairMember][0] = x;
+                        keyPos[pairMember][1] = y;
+                    }
+                }
+            }
+        }
+        //Käydään Playfair-salauksen säännöt läpi
+        //Merkit eivät voi olla samoja, se tarkistetaan paria muodostaessa
+        //Merkit ovat samalla rivillä
+        /*if (keyPos[0][0] == keyPos[1][0]) {
+            for (int i = 0; i < 2; i++) {
+            }
+        }*/
+        return cipherPair;
+    }
+
+    private boolean cipherable(char c) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (c == alphabet[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ dokumentaatio/kayttoohjeet.md
@@ -0,0 +1,11 @@
+Ohjelmalla voidaan toteuttaa yksinkertaisia salauksia kirjoitetulle tekstille.
+Ohjelman toiminta:
+NYI 1. Valitaan salaamisen ja purkamisen välillä (toistaiseksi vain salaus on mahdollista)
+2. Valitaan salausmuoto (toistaiseksi vain Caesar)
+3. Syötetään avain
+4. Syötetään viesti
+Ohjelma tulostaa avaimen, viestin ja salatun viestin.
+
+Huomioitavaa:
+Ohjelma lukee vain pieniä kirjaimia välillä a-z, ei mitään muuta.
+Ohjelma ei salaa esimerkiksi välilyöntejä, erikoismerkkejä tai isoja kirjaimia.
