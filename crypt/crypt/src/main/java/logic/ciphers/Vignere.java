ff80063d45dab659f4b48b5bd58f5d1749b8647d
korppi <korppi@users.noreply.github.com>
2015-01-11 23:15:22 +0200

    Viimeiset lisäykset

---
+++ crypt/crypt/src/main/java/logic/ciphers/Vignere.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2015 korppi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package logic.ciphers;
+
+/**
+ *
+ * @author korppi
+ *
+ * Luokka toteuttaa Vignere-salauksen
+ * @see logic.ciphers.Cipher
+ */
+public class Vignere extends Cipher {
+
+    private int[] keyInts;
+    private int keyPos;
+    private Caesar caesar;
+
+    public Vignere(Caesar c) {
+        keyInts = new int[]{0};
+        key = new char[]{' '};
+        caesar = c;
+    }
+
+    @Override
+    public char encrypt(char plain) {
+        char result = caesar.caesarShift(plain, keyInts[keyPos]);
+        keyPos++;
+        if (keyPos >= keyInts.length) {
+            keyPos = 0;
+        }
+        return result;
+    }
+
+    @Override
+    public char decrypt(char cipher) {
+        char result = caesar.caesarShift(cipher, -keyInts[keyPos]);
+        keyPos++;
+        if (keyPos >= keyInts.length) {
+            keyPos = 0;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean setKey(char c[]) {
+        int newKeyInts[] = new int[c.length];
+        for (int i = 0; i < c.length; i++) {
+            boolean validChar = false;
+            for (int j = 0; j < alphabet.length; j++) {
+                if (c[i] == alphabet[j]) {
+                    validChar = true;
+                    newKeyInts[i] = j + 1;
+                    break;
+                }
+            }
+            if (!validChar) {
+                return false;
+            }
+        }
+        key = c;
+        keyInts = newKeyInts;
+        keyPos = 0;
+        return true;
+    }
+
+    @Override
+    public char[] getKey() {
+        return key;
+    }
+}
---
+++ crypt/crypt/src/test/java/logic/ciphers/VignereTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2015 korppi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package logic.ciphers;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author korppi
+ */
+public class VignereTest {
+
+    private Vignere v;
+    private char[] plain;
+    private char[] result;
+    private char[] cipher;
+    private char[] key;
+
+    @Before
+    public void setUp() {
+        v = new Vignere(new Caesar());
+        plain = new char[]{'z', 'b', 'c', 'd'};
+        cipher = new char[]{'a', 'b', 'd', 'd'};
+        key = new char[]{'a', 'z'};
+        result = new char[4];
+        v.setKey(key);
+    }
+
+    @Test
+    public void testGetKey() {
+        assertArrayEquals(v.getKey(), key);
+    }
+
+    @Test
+    public void testSetKey() {
+        char[] c = {'a', 'b'};
+        v.setKey(c);
+        assertArrayEquals(v.getKey(), c);
+    }
+
+    @Test
+    public void testInvalidKey() {
+        v.setKey("25".toCharArray());
+        assertArrayEquals(v.getKey(), key);
+    }
+
+    @Test
+    public void testEncryptLoop() {
+        result[0] = v.encrypt(plain[0]);
+        assertEquals(result[0], cipher[0]);
+    }
+
+    @Test
+    public void testDecryptLoop() {
+        result[0] = v.decrypt(cipher[0]);
+        assertEquals(result[0], plain[0]);
+    }
+
+    @Test
+    public void testEncryptKeyLoop() {
+        result = new char[plain.length];
+        for (int i = 0; i < plain.length; i++) {
+            result[i] = v.encrypt(plain[i]);
+        }
+        assertArrayEquals(result, cipher);
+    }
+
+    @Test
+    public void testDecryptKeyLoop() {
+        result = new char[cipher.length];
+        for (int i = 0; i < cipher.length; i++) {
+            result[i] = v.decrypt(cipher[i]);
+        }
+        assertArrayEquals(result, plain);
+    }
+}
